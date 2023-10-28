9c3dda8b26cf42e360577e545f05404f96222245
Alex <alexandrkibalka@gmail.com>
2023-10-28 14:01:29 +0300

    hw3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector(".feedback-form");
+const localStorageKey = "feedback-form-state";
+
+const savedFormData = JSON.parse(localStorage.getItem(localStorageKey)) || {};
+form.elements.email.value = savedFormData.email || "";
+form.elements.message.value = savedFormData.message || "";
+
+const saveToLocalStorage = throttle(() => {
+  const formData = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+}, 500);
+
+form.addEventListener("input", saveToLocalStorage);
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+  console.log(savedFormData);
+});
